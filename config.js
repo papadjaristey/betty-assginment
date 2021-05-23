a23075c7ad8c7bc3b5dfd714afe5d279f08975ab
Alexander Vovchenko <papdjaristey@gmail.com>
2021-05-24 01:50:07 +0300

    rework variables storage to function based config

---
+++ config.js
@@ -0,0 +1,8 @@
+export const userInfo = {
+
+    getFirstName: (timeStamp) => `Random${timeStamp}`,
+    getLastName: (timeStamp) => `Jack${timeStamp}`,
+    getEmailAddress: (timeStamp) => `jack${timeStamp}@abc.com`,
+    pwd: 'MyPwd12345'
+
+}
--- variables.js
+++
@@ -1,8 +0,0 @@
-export var userInfo = {
-
-    fName: `Random${Date.now()}`,
-    lName: `Jack${Date.now()}`,
-    eAddress: `jack${Date.now()}@abc.com`,
-    pwd: 'MyPwd12345'
-
-}

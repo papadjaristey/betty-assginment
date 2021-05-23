4fdd41818ab1b447cbbfbf3249c93bc2558f898f
Alexander Vovchenko <papdjaristey@gmail.com>
2021-05-24 01:54:15 +0300

    Add selectors for Story 3 (avatar upload) test

--- pages/main.js
+++ pages/main.js
@@ -1,10 +1,19 @@
 import {Selector} from 'testcafe';
+import * as sharedSelectors from '../sharedSelectors';
 
 class MainPage {
+    constructor () {
+        this.myAccountButton = Selector('a').withAttribute('href', '/my-account');
+        this.uploadButton = Selector('span').withAttribute('role', 'button');
+        this.uploader = Selector('input').withAttribute('type', 'file');
+        this.submitButton = sharedSelectors.submitButton;
+        this.uploadedAvatar = Selector('p').withText('ryu_avatar.jpg');
+        this.avatarURL = Selector('img').withAttribute('src', /ryu_avatar.jpg$/);
+        this.changesSavedMessage = Selector('div').withAttribute('class','MuiAlert-message').withText('Profile successfully updated');
+    }
     getWelcomeHeader (fName, lName) {
         return Selector('h3').withText(new RegExp(`Welcome.{2}${fName} ${lName}`))
     }
 }
 
 export const mainPage = new MainPage();
-

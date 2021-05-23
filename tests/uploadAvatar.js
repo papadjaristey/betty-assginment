a5bcbf71a05c1834c4b51d16f4db330d97e24b42
Alexander Vovchenko <papdjaristey@gmail.com>
2021-05-24 02:01:55 +0300

    Add Story 3 (personalize accout with avatar) test coverage

---
+++ tests/uploadAvatar.js
@@ -0,0 +1,30 @@
+import {registerPage} from '../pages/registeration';
+import {loginPage} from '../pages/login';
+import {mainPage} from '../pages/main';
+import {Selector} from 'testcafe'
+
+fixture `Story 3`
+    .page `https://ui-test-app.betty.app/login`;
+
+test
+    .before(async (t) => {
+        t.timeStamp = Date.now();
+        await registerPage.registerNewUser(t.timeStamp);
+        await loginPage.login(t.timeStamp);
+    })
+    ('As a user I want to change my avatar, so that I can personalize my account', async t => {
+        await t
+            .click(mainPage.myAccountButton)
+            .setFilesToUpload(mainPage.uploader, [
+                '../ryu_avatar.jpg'
+            ])
+            .click(mainPage.uploadButton)
+            .click(mainPage.submitButton)
+            .expect(mainPage.uploadedAvatar.exists).ok('Avatar image not uploaded')
+            .click(mainPage.submitButton)
+            .expect(mainPage.changesSavedMessage.exists).ok('Changes not saved');
+        await t.eval(() => location.reload(true));
+        await t
+            .expect(mainPage.avatarURL.exists).ok('Avatar not uploaded')
+
+    });

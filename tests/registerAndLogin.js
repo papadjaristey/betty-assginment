adfeac64c67dc4b852934bacafd0df42e42a8e9d
Alexander Vovchenko <papdjaristey@gmail.com>
2021-05-24 01:56:58 +0300

    Rework Story 1 (register and login) test using functions

--- tests/registerAndLogin.js
+++ tests/registerAndLogin.js
@@ -1,23 +1,18 @@
 import {registerPage} from '../pages/registeration';
 import {loginPage} from '../pages/login';
 import {mainPage} from '../pages/main';
-
-const {userInfo} = require('../variables')
+import {userInfo} from '../config';
 
 fixture `Story 1`
     .page `https://ui-test-app.betty.app/login`;
 
 test('As a new customer I want to register a new account, so that I can use the application', async t => {
+    t.timeStamp = Date.now();
+    await registerPage.registerNewUser(t.timeStamp);
     await t
-        .click(loginPage.registerButton)
-        .typeText(registerPage.firstNameInput, userInfo.fName)
-        .typeText(registerPage.lastNameInput, userInfo.lName)
-        .typeText(registerPage.emailAddressInput, userInfo.eAddress)
-        .typeText(registerPage.passwordInput, userInfo.pwd)
-        .click(registerPage.submitButton)
-        .expect(loginPage.createdMessage.exists).ok('Account not created')
-        .typeText(loginPage.emailAddressInput, userInfo.eAddress)
-        .typeText(loginPage.passwordInput, userInfo.pwd)
-        .click(loginPage.submitButton)
-        .expect(mainPage.getWelcomeHeader(userInfo.fName, userInfo.lName).exists).ok('Login failed')
+        .expect(loginPage.createdMessage.exists).ok('Account not created');
+    await loginPage.login(t.timeStamp);
+    await t
+        .expect(mainPage.getWelcomeHeader(userInfo.getFirstName(t.timeStamp), userInfo.getLastName(t.timeStamp)).exists).ok('Login failed');
+
 });

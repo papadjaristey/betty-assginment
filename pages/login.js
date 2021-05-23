230305e9f9db25d2696c927e7e9e8a689d118b2a
Alexander Vovchenko <papdjaristey@gmail.com>
2021-05-24 01:51:56 +0300

    Add login function.

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,22 @@
-import {Selector} from 'testcafe';
+import {Selector, t} from 'testcafe';
 import * as sharedSelectors from '../sharedSelectors';
+import {userInfo} from '../config';
 
 class LoginPage {
     constructor() {
         this.emailAddressInput = sharedSelectors.emailAddressInput;
-        this.passwordInput =     sharedSelectors.passwordInput;
-        this.submitButton =      sharedSelectors.submitButton;
-        this.registerButton =    Selector('a').withAttribute('href', '/new-account');        
-        this.createdMessage =    Selector('div').withAttribute('class', 'MuiAlert-message').withText('Your account has been created, you can now login here');
+        this.passwordInput = sharedSelectors.passwordInput;
+        this.submitButton = sharedSelectors.submitButton;
+        this.registerButton = Selector('a').withAttribute('href', '/new-account');        
+        this.createdMessage = Selector('div').withAttribute('class', 'MuiAlert-message').withText('Your account has been created, you can now login here');
         this.wrongCredsMessage = Selector('div').withAttribute('class', 'MuiAlert-message').withText('You\'ve entered a wrong email address and/or password');
 
+    }
+    async login(timeStamp) {
+        await t
+            .typeText(loginPage.emailAddressInput, userInfo.getEmailAddress(timeStamp))
+            .typeText(loginPage.passwordInput, userInfo.pwd)
+            .click(loginPage.submitButton);
     }
 }
 

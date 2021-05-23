1964549dd9db0b771c6d67f79e755b9341503ee4
Alexander Vovchenko <papdjaristey@gmail.com>
2021-05-24 01:52:55 +0300

    Add register new user function

--- pages/registeration.js
+++ pages/registeration.js
@@ -1,15 +1,26 @@
-import {Selector} from 'testcafe';
+import {Selector, t} from 'testcafe';
+import {loginPage} from './login';
 import * as sharedSelectors from '../sharedSelectors';
+import {userInfo} from '../config'
 
 class RegisterPage {
     constructor() {
-        this.firstNameInput =    Selector('input').withAttribute('name', 'first_name');
-        this.lastNameInput =     Selector('input').withAttribute('name', 'last_name');
+        this.firstNameInput = Selector('input').withAttribute('name', 'first_name');
+        this.lastNameInput = Selector('input').withAttribute('name', 'last_name');
         this.emailAddressInput = sharedSelectors.emailAddressInput;
-        this.passwordInput =     sharedSelectors.passwordInput;        
-        this.submitButton =      sharedSelectors.submitButton;
-        this.createdMessage =    Selector('div').withAttribute('class','MuiAlert-message').withText('Your account has been created, you can now login here')
+        this.passwordInput = sharedSelectors.passwordInput;        
+        this.submitButton = sharedSelectors.submitButton;
+        this.createdMessage = Selector('div').withAttribute('class','MuiAlert-message').withText('Your account has been created, you can now login here');
         
+    }
+    async registerNewUser(timeStamp) {
+        await t
+            .click(loginPage.registerButton)
+            .typeText(this.firstNameInput, userInfo.getFirstName(timeStamp))
+            .typeText(this.lastNameInput, userInfo.getLastName(timeStamp))
+            .typeText(this.emailAddressInput, userInfo.getEmailAddress(timeStamp))
+            .typeText(this.passwordInput, userInfo.pwd)
+            .click(this.submitButton);
     }
 }
 
